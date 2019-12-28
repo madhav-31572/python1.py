349f5fc4d89b09b24e4f92e81a38c18559c49e70
madhav-31572 <59111052+madhav-31572@users.noreply.github.com>
2019-12-28 15:17:21 +0530

    Add files via upload

---
+++ samplepython11.py
@@ -0,0 +1,12 @@
+  
+def maximum(a, b, c): 
+  
+    if (a >= b) and (a >= b): 
+        largest = a 
+  
+    elif (b >= a) and (b >= a): 
+        largest = b 
+    else: 
+        largest = c 
+          
+    return largest 
